69d92fe97f78f8504b8f0e1ac52a4e4150b44bb6
Mohamed Alganzory <vidyatuts@gmail.com>
2021-11-18 13:54:51 +0800

    combined user-Model files, added, updated, fixed, its functions
    combined the previously called user-service with the user model and now you need to require only this file to access the model as well as the functions, fixed bugs related to async and connecting to the db

--- models/user-Model.js
+++ models/user-Model.js
@@ -2,23 +2,86 @@ let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    id: {
-        type: String,
-        default: null,
-    },
-    email: {
-        type: String,
-        required: [true, "email required"],
-        unique: [true, "email already registered"],
-    },
-    username: String,
-    profilePhoto: String,
-    password: String,
+  id: {
     type: String,
-    source: { type: String, required: [true, "source not specified"] },
-    lastVisited: { type: Date, default: new Date() }
+    default: null,
+  },
+  email: {
+    type: String,
+    required: [true, "email required"],
+    unique: [true, "email already registered"],
+  },
+  username: String,
+  profilePhoto: String,
+  password: String,
+  type: String,
+  source: { type: String, required: [true, "source not specified"] },
+  lastVisited: { type: Date, default: new Date() },
 });
+const User = mongoose.model("user", userSchema, "user");
+// var userModel = mongoose.model("user", userSchema, "user");
 
-var userModel = mongoose.model("user", userSchema, "user");
- 
-module.exports=userModel;
+exports.userModel = User;
+
+exports.addUser = async (user) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    const newUser = new User(user);
+    await newUser.save();
+    return newUser;
+  } catch (error) {
+    console.error(error);
+  }
+};
+exports.getUsers = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    return await User.find({});
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.getUserByEmail = async (email) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    return await User.findOne({ email });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.getUserById = async (id) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    return await User.findOne({ id });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.getUserByUsername = async (username) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    return await User.findOne({ username });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.findOrCreate = async (id, user) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    const currentUser = await User.findOne({ id });
+    if (!currentUser) {
+      const newUser = await exports.addUser(user);
+      return newUser;
+    } else {
+        console.log ("google account already registered, logging in instead..");
+      return currentUser;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
