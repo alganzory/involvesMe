5e552ad4550d3c0cae72229f95279c5d6fb1b7ab
Mohamed Alganzory <vidyatuts@gmail.com>
2021-11-18 13:35:02 +0800

    update landing route
    rename landing route, add authentication to it, and fetch user data

---
+++ routes/landing-route.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const {checkAuthenticated, checkNotAutheticated} = require('../controllers/authController');
+router.get('/', checkAuthenticated, (req, res) => {
+   // this is temportary until landing page is created
+    res.send (`<h1> landing page </h1>
+    </br>   
+    </br>
+    <h3> email: ${req.user.email} </h3>
+    </br>
+    <h3>
+    username: ${req.user.username}
+    </h3>
+    </br>
+    <h3>
+    source: ${req.user.source}
+    <h3>
+    </br>
+    <h3>
+    password: ${req.user.password}
+    <h3>
+    </br>
+    <form method="POST" action="/auth/logout" >  <button type="submit">Logout</button> <form>
+    `)
+});
+
+module.exports = router;
