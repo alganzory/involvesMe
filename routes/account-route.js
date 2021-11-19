f9196d5deb68ebf4cf478870a57d5cc2d54dc768
Omar <omar0alaa@gmail.com>
2021-11-19 13:02:44 +0800

    Added account controller(not fully complete) and route for username update

--- routes/account-route.js
+++ routes/account-route.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const methodOverride = require('method-override');
 
+const accountController = require('../controllers/accountController');
 
 const {checkAuthenticated} = require('../controllers/authController');
 
-
-router.get("/", checkAuthenticated, (req, res) => {
-        res.render("my-account", { title: "My Account" });
-});
-
+router.get("/", checkAuthenticated, accountController.get_myaccount);
+router.post("/username", checkAuthenticated,accountController.change_username);
 
 module.exports = router;
