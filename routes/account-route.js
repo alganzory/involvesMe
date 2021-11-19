5cb701fdd9ebd6c206a5eaf3d326c917ed5cdc6a
Omar <omar0alaa@gmail.com>
2021-11-19 13:06:13 +0800

    Revert "Added account controller(not fully complete) and route for username update"
    This reverts commit f9196d5deb68ebf4cf478870a57d5cc2d54dc768.

--- routes/account-route.js
+++ routes/account-route.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const methodOverride = require('method-override');
 
-const accountController = require('../controllers/accountController');
 
 const {checkAuthenticated} = require('../controllers/authController');
 
-router.get("/", checkAuthenticated, accountController.get_myaccount);
-router.post("/username", checkAuthenticated,accountController.change_username);
+
+router.get("/", checkAuthenticated, (req, res) => {
+        res.render("my-account", { title: "My Account" });
+});
+
 
 module.exports = router;
