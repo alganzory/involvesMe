5a4bfa5e5ce703f26bd314ad57917171f9bc9d54
Xiao-Daffy <fujisan521@gmail.com>
2021-11-26 17:36:35 +0800

    changed the effect of the swiper slide displaying

--- assets/js/posts_swiper.js
+++ assets/js/posts_swiper.js
@@ -1,19 +1,10 @@
-var swiper = new Swiper('.swiper-container', {
-  pagination: '.swiper-pagination',
-  effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  coverflowEffect: {
-    rotate:10,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows : true
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4, //the number of post cards showing per view
+  spaceBetween: 10,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
-  loop: true,
-  autoplay: {
-    delay: 1500,//slides shifting time
-    disableOnInteraction: false
-  },
+  loop: true
 });
