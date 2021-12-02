22d65bbad19582e64ce82bdb0106fd137d177a3c
Xiao-Daffy <fujisan521@gmail.com>
2021-12-02 14:30:06 +0800

    adjusted the displaying of the number of the post cards based on the screen size

--- assets/js/posts_swiper.js
+++ assets/js/posts_swiper.js
@@ -1,16 +1,28 @@
+
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 2, //the number of post cards showing per view
+  speed: 600,
+loop: true,
+autoplay: {
+delay: 1500,//slides shifting time
+disableOnInteraction: false
+},
+slidesPerView: 'auto',
+pagination: {
+el: '.swiper-pagination',
+type: 'bullets',
+clickable: true
+},
+breakpoints: {
+  //screen size, when below 320, then show one slide per view
+320: {
+  slidesPerView: 1,
+  spaceBetween: 40
+},
+
+// //screen size, when up to 960, then show 2 slides per view
+960: {
+  slidesPerView: 2,
   spaceBetween: 10,
-  freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    type: 'bullets',
-    clickable: true,
-  },
-  
-  autoplay: {
-    delay: 3000,//slides shifting time
-    disableOnInteraction: false
-  },
-  loop: true,
+}
+}
 });
