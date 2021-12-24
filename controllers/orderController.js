b06e532048780ebd1de9a71b148b6024d9edd18f
Xiao-Daffy <fujisan521@gmail.com>
2021-12-24 16:38:39 +0800

    updated the refund part for cancle order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,73 +1,9 @@
 const orderService = require("../models/order-model");
-
+const UserService = require("../models/user-Model")
 const ProductService = require("../models/product-model")
 const uuid = require("uuid");
 const e = require("express");
 
-
-
-//cancel order
-//1. get the order_id from frontend(input hidde) using req.body.orderId and userId(req.user.id)
-//2. find the oder info from database by orderId(and userId) --> get the order payment(money), change the status for the order("cancelled")
-//3. for loop to get the ordered products while for each i 
-//3.1 get the productId(meanwhile get the product by productId, and get the stock), product quantity(need to restock the product stock), 
-//upadate the stock (stock + quantity)
-//4. refund to the user
-// edit product
-const editProductFromCart = async(req, res)=>{
-    
-    var cartSearch = await cartService.getCartByuserId(req.user.id);//get cart
-    
-
-    var edit_id = req.body.productId;//
-    console.log("product id: "+edit_id)
-    var origanProduct =await ProductService.getProductById(edit_id);
-    var origanProduct_stock =origanProduct.stock;
-
-    var quantity = req.body.newQuantity;//test 
-    console.log("stock: "+origanProduct_stock)
-    console.log("quantity: "+quantity)
-    console.log(origanProduct_stock);
-    
-      if(quantity<=origanProduct_stock && quantity>0){
-        for (let index = 0; index < cartSearch.products.length; index++) {
-            if (cartSearch.products[index].product == edit_id) {
-                cartSearch.totalPrice = Number(cartSearch.totalPrice) - Number(cartSearch.products[index].totalPrice);
-                
-                  cartSearch.totalPrice = Number(cartSearch.totalPrice) - Number(cartSearch.products[index].totalPrice);
-                cartSearch.products[index].totalPrice = quantity *  Number(cartSearch.products[index].price);
-                 cartSearch.products[index].quantity= quantity
-              
-                cartSearch.totalPrice = Number(cartSearch.totalPrice) + Number(cartSearch.products[index].totalPrice);
-             
-                var cart = {
-                    userId: cartSearch.userId,
-                    id: cartSearch.id,
-                    products: cartSearch.products,
-                    totalPrice: cartSearch.totalPrice,
-                };
-                await cartService.updateCart(req.user.id, cart)
-            }
-        }
-         
-        console.log(cartSearch)
-     }else{
-        
-        req.flash(
-            'outstock',
-            "out of stock,current stock is: "+origanProduct_stock
-          );
-      
-     }
-
-    
-   
-   
-     console.log('edited')
-      res.redirect("/cart/");
-    
-      
-}
 
 const cancelOrder = async (req, res) => {
    
@@ -77,11 +13,13 @@ const cancelOrder = async (req, res) => {
 
     //search the order
     var orderSearch = await orderService.getOrderById(orderId);
+    console.log(orderSearch);
+
     var orderStatus = orderSearch.orderStatus;
     console.log("order status: "+orderStatus);
 
     //cancel order only for pending orders -->restore the product,
-    if(orderSearch.orderStatus == "pending"){
+    if(orderSearch.orderStatus == "pending" || orderSearch.orderStatus == "Pending"){
         console.log("ready to cancel");
         for(let i =0; i< orderSearch.products.length; i++){
 
@@ -103,27 +41,36 @@ const cancelOrder = async (req, res) => {
 
         }
 
-        // change the status of the order
-        var canceledOrder = await orderService.updateOrderStatus(orderId, "canceled")
-
-        console.log(canceledOrder);
-        //refund
+         //3. refund
         var totalPrice = orderSearch.totalPrice;
-        //to be continued....
+        var reward = orderSearch.reward;
+        var pointsUsed = orderSearch.pointsUsed;
 
+        //3.1 get buyer info
+        var buyer = await UserService.getUserById(req.user.id);
         
+        //3.2 update the points
+        var upatedPoints = Number(buyer.points)-Number(reward)+Number(pointsUsed);
+
+        await UserService.updatePoints(req.user.id, upatedPoints);
+
+        //3.3 update the balance
+        var updatedBalance = Number(buyer.balance)+Number(totalPrice);
+        await UserService.updateBalance(req.user.id, updatedBalance);
+
+        var updatebuyer = await UserService.getUserById(req.user.id);
+        console.log("updated buyer")
+        console.log(updatebuyer)
+  
+        //4. change the status of the order
+        var canceledOrder = await orderService.updateOrderStatus(orderId, "canceled")
+        console.log(canceledOrder)
     }else{
         req.flash(
             "cannotcancel",
             "Sorry! Order cannot be canceled, because the order is "+orderStatus
         )
     }
-
-
-
-    // console.log(orderSearch);
-
-
 
     res.redirect("/order/")
 };
