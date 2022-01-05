4aebc4dbffedc8815c69f918945cc29535064313
alaaAlrhman <81565770+alaaAlrhman@users.noreply.github.com>
2022-01-05 15:43:44 +0800

    creating a responsive landing page

--- routes/landing-route.js
+++ routes/landing-route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { checkAuthenticated, checkNotAutheticated } = require('../controllers/authController');
 router.get('/', checkAuthenticated, (req, res) => {
     // this is temportary until landing page is created
-
+    res.render('landing')
     res.send(`<h1> landing page </h1>
     </br>   
     </br>
