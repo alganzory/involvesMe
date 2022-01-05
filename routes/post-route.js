2bce2d434e3a97f5702a442d498069a11e608b38
Xiao-Daffy <fujisan521@gmail.com>
2022-01-06 03:33:36 +0800

    added route for editing and deleting posts

--- routes/post-route.js
+++ routes/post-route.js
@@ -5,6 +5,8 @@ const postController = require("../controllers/postController")
 const { checkAuthenticated, checkNotAutheticated } = require('../controllers/authController');
 
 router.get('/:id', checkAuthenticated,postController.get_product);
-  
 
+router.post('/delete', checkAuthenticated, postController.deletePost);
+
+router.post('/edit', checkAuthenticated, postController.editPost);
 module.exports=router;
