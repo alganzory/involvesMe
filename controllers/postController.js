30f573e270edae896fe416e29153a97cd3a376f7
Omar <omar0alaa@gmail.com>
2022-01-06 08:24:57 +0800

    added add Post backend and fixed bug related to post in profile

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,22 +8,37 @@ const get_userposts = async (id) => {
 const get_product = async (req, res) => {
     const postId = req.params.id;
     const post = await PostService.getPostById(postId);
-    
+
     console.log(post)
     var user = {
         id: req.user.id
     }
-    res.render("viewPost", { post: post,user: user,title: "Post"});
-  };
+    res.render("viewPost", { post: post, user: user, title: "Post" });
+};
 
+// Function Called from Profile Controller
 const addUserPost = async (post) => {
     PostService.addPost(post)
 };
+const makePost = async (req, res) => {
+    var post = {
+        userId: req.user.id,
+        title: req.body.title,
+        content: req.body.description,
+    };
+    console.log(req.body)
+    await PostService.addPost(post)
+    res.redirect("/profile/me");
+};
 
-
+const get_makePost = async (req, res) => {
+    res.render("addPost", { title: "Add Post" });
+};
 
 module.exports = {
     get_userposts,
     addUserPost,
-    get_product
+    get_product,
+    makePost,
+    get_makePost
 }
--- routes/post-route.js
+++ routes/post-route.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const postController = require("../controllers/postController")
 const { checkAuthenticated, checkNotAutheticated } = require('../controllers/authController');
-
+router.get('/addPost', checkAuthenticated,postController.get_makePost);
 router.get('/:id', checkAuthenticated,postController.get_product);
-  
 
+router.post('/makePost', checkAuthenticated,postController.makePost);
 module.exports=router;
