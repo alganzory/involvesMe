b25615ef65a7777cd60ec1e3f75ec88eb8f927d4
alaaAlrhman <81565770+alaaAlrhman@users.noreply.github.com>
2022-01-16 22:57:23 +0800

    nav sectiob

--- routes/landing-route.js
+++ routes/landing-route.js
@@ -5,27 +5,7 @@ const { checkAuthenticated, checkNotAutheticated } = require('../controllers/aut
 router.get('/', checkAuthenticated, (req, res) => {
     // this is temportary until landing page is created
     res.render('landing')
-    res.send(`<h1> landing page </h1>
-    </br>   
-    </br>
-    <h3> email: ${req.user.email} </h3>
-    </br>
-    <h3>
-    username: ${req.user.username}
-    </h3>
-    </br>
-    <h3>
-    source: ${req.user.source}
-    <h3>
-    </br>
-    <h3>
-    password: ${req.user.password}
-    <h3>
-    </br>
-    <form method="POST" action="/auth/logout" >  <button type="submit">Logout</button> </form>
-    <form method="GET" action="/settings" >  <button type="submit">Account</button> </form>
-    <form method="GET" action="/profile/me" >  <button type="submit">Profile</button> </form>
-    `)
+
 });
 
 module.exports = router;
